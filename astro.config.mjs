2363dec9c050ad76a471f56e05a5afb351da7663
alt-del-code <nithishkrishna852@gmail.com>
2024-12-05 12:32:27 +0530

    Enhance Astro project configuration and dependencies
    - Updated astro.config.mjs to include multiple integrations: Tailwind, React, Markdoc, Sitemap, Partytown, and Keystatic.
    - Configured Vite with custom rollup options and increased chunk size warning limit.
    - Modified package.json to add new dependencies for Markdoc, Node adapter, Partytown, React, and Tailwind, along with their respective versions.
    - Added devDependencies for rollup-plugin-visualizer and vite-plugin-inspect.
    - Updated tsconfig.json to support React JSX syntax.
    
    These changes improve the project's build process and enhance its functionality.

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,5 +2,53 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import inspect from 'vite-plugin-inspect';
+import { visualizer } from 'rollup-plugin-visualizer';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import markdoc from '@astrojs/markdoc';
+import sitemap from '@astrojs/sitemap';
+import partytown from '@astrojs/partytown';
+
+
+import keystatic from '@keystatic/astro'
+
+import node from '@astrojs/node';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react(),
+    markdoc(),
+    sitemap(),
+    partytown(),
+    keystatic()
+  ],
+
+  output: 'static',
+  site: 'https://www.krrishco.com',
+
+  adapter: node({
+    mode: 'standalone',
+  }),
+
+
+  vite: {
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            'vendor': ['react', 'react-dom'],
+            'utils': ['lodash'],
+          },
+        },
+      },
+      chunkSizeWarningLimit: 1000, // Increase the limit to 1 MB
+    },
+    plugins: [
+      inspect(),
+      visualizer(),
+    ],
+  },
+});
